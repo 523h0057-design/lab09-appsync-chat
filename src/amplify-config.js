4139b2ddc02a8d1850534cc8a69e911e02f15a68
523h0057-design <523h0057@student.tdtu.edu.vn>
2025-11-29 12:26:37 +0700

    remove exposed API key

--- src/amplify-config.js
+++ src/amplify-config.js
@@ -5,7 +5,7 @@ Amplify.configure({
   aws_appsync_graphqlEndpoint: import.meta.env.VITE_APPSYNC_ENDPOINT,
   aws_appsync_region: import.meta.env.VITE_APPSYNC_REGION,
   aws_appsync_authenticationType: "API_KEY",
-  aws_appsync_apiKey: import.meta.env.VITE_APPSYNC_APIKEY,
+  aws_appsync_apiKey: import.meta.env.VITE_APPSYNC_APIKEY
 });
 
 // console.log("CONFIG LOADED!!!");
