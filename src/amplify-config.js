2953cb6fb286084ee588a9415cc08262e72817b3
523h0057-design <523h0057@student.tdtu.edu.vn>
2025-11-29 11:26:55 +0700

    update amplify-config with env variables

--- src/amplify-config.js
+++ src/amplify-config.js
@@ -1,15 +1,23 @@
+// src/amplify-config.js
 import { Amplify } from "aws-amplify";
 
-console.log("CONFIG LOADED!!!");
-
 Amplify.configure({
-  aws_project_region: "ap-southeast-1",
-  aws_appsync_graphqlEndpoint:
-    "https://6t5ycaholve2fdtzwehaw5onau.appsync-api.ap-southeast-1.amazonaws.com/graphql",
-  aws_appsync_region: "ap-southeast-1",
+  aws_appsync_graphqlEndpoint: import.meta.env.VITE_APPSYNC_ENDPOINT,
+  aws_appsync_region: import.meta.env.VITE_APPSYNC_REGION,
   aws_appsync_authenticationType: "API_KEY",
-  aws_appsync_apiKey: "da2-brs7jchdfbc3jticdgzr6kdia",
+  aws_appsync_apiKey: import.meta.env.VITE_APPSYNC_APIKEY,
 });
+
+// console.log("CONFIG LOADED!!!");
+
+// Amplify.configure({
+//   aws_project_region: "ap-southeast-1",
+//   aws_appsync_graphqlEndpoint:
+//     "https://6t5ycaholve2fdtzwehaw5onau.appsync-api.ap-southeast-1.amazonaws.com/graphql",
+//   aws_appsync_region: "ap-southeast-1",
+//   aws_appsync_authenticationType: "API_KEY",
+//   aws_appsync_apiKey: "da2-brs7jchdfbc3jticdgzr6kdia",
+// });
 
 // import { Amplify } from "aws-amplify";
 
